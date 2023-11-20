c15b04aa71289ec2e32913ccf2ed5297d76e517d
IvanSalovProg <130348846+IvanSalovProg@users.noreply.github.com>
2023-11-21 00:37:26 +0500

    1.20.11.03

--- Goods/Goods/Migrations/GoodsAppContextModelSnapshot.cs
+++ Goods/Goods/Migrations/GoodsAppContextModelSnapshot.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
+
 #nullable disable
 
 namespace Goods.Migrations
